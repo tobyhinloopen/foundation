8c45978c5378285807e5f9cb20340846182d8357
zurbchris <chris@zurb.com>
2012-05-31 16:53:30 -0700

    updated typography

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 $:.unshift File.dirname(__FILE__)
 require 'convert-number-to-word'
+require 'modular-scale'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
